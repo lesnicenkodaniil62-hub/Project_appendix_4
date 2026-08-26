0129d63244431f1fafd6f4dc761222e3d9008e5c
Daniil <lesnicenkodaniil62@gmail.com>
2026-08-26 12:09:17 +0300

    feature_1 создание нового приложение под названием catalog

--- catalog/views.py
+++ catalog/views.py
@@ -1,13 +1,14 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
     """Контроллер для отображения домашней страницы."""
-    context = {"success": False}
-    return render(request, "catalog/home.html", context)
+    # render автоматически возвращает HttpResponse, mypy это знает благодаря django-stubs
+    return render(request, "catalog/home.html")
 
 
-def contacts(request):
+def contacts(request: HttpRequest) -> HttpResponse:
     """Контроллер для отображения страницы с контактной информацией."""
     context = {"success": False}
 
@@ -15,6 +16,7 @@ def contacts(request):
         name = request.POST.get("name")
         phone = request.POST.get("phone")
         message = request.POST.get("message")
+
         print(f"Получено сообщение от {name} ({phone}): {message}")
         context["success"] = True
 
