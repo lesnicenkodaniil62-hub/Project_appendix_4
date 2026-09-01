253e214bfa43b578dc3ccd32ce1500469c7630ad
Daniil <lesnicenkodaniil62@gmail.com>
2026-09-01 18:07:46 +0300

    feature_2 создание суперпользователя или admin в catalog

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """Админ-панель для модели Category."""
+
+    list_display = ("id", "name")
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    """Админ-панель для модели Product."""
+
+    list_display = ("id", "name", "price", "category")
+    list_filter = ("category",)
+    search_fields = ("name", "description")
+
